e5e3208f406087c107afc41edb040cef41150a44
AltF4Max <140857759+AltF4Max@users.noreply.github.com>
2025-04-02 20:57:15 +0300

    Update config.go

--- config/config.go
+++ config/config.go
@@ -1,4 +1,4 @@
 package config
 
-const ApiKey = "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"
+const ApiKey = "************************************"
 const Url = "https://openrouter.ai/api/v1/chat/completions"
